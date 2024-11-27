e70501891fd0f36499000f57be39efe932ea6ba2
Bharath kumar <bharath_web@hotmail.com>
2024-11-27 23:02:25 +0530

    no change with previous file, but git says so

--- python-scripts/flask_mysql_app/app.py
+++ python-scripts/flask_mysql_app/app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 # Configure db
 db = yaml.load(open('db.yaml'), Loader=yaml.SafeLoader)
-print(db)
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
